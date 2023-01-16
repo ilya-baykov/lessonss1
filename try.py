e7fe38f35c6f787a4f347c95e6914cc1edb092b8
ilya-baykov <i-baykov@internet.ru>
2023-01-16 03:11:18 +0300

    Fixed a bug with the function of changing the name of the student Added Function "transfer_group" which allows you to transfer a student to another group

--- try.py
+++ try.py
@@ -29,9 +29,19 @@ class Students:
             if self.__name in Students.__data[self.__group]:
                 index = Students.__data[self.__group].index(self.__name)
                 Students.__data[self.__group][index] = value
+                self.__name = value
             else:
                 raise ValueError("Такой студент не найден")
         else:
             raise ValueError("Недопустимый формат")
 
+    def transfer_group(self, new_group):
+        if new_group in Students.__data:
+            Students.__data[self.__group].remove(self.__name)
+            Students.__data[new_group].append(self.__name)
+        else:
+            raise ValueError("Такой группы не существует")
+
     name = property(get_name_student, set_new_name_student)
+
+
